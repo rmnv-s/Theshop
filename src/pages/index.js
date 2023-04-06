c1ab0affede79f294a87a8bfebb0c0f51bccae1d
dev <remnev.dev@gmail.com>
2023-04-06 12:35:14 +0000

    feat: add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
 import '../styles/index.scss';
+
+const cartInfo = document.querySelector('.cart-info');
+const content = document.querySelector('.content');
+const close = document.querySelector('.cart-info__close');
+
+cartInfo.addEventListener('click', () => {
+  content.classList.add('content__opened');
+  content.classList.remove('content__close');
+});
+
+close.addEventListener('click', () => {
+  content.classList.remove('content__opened');
+  content.classList.add('content__close');
+});
