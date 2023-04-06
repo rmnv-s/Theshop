a2eb3cb52bf8f192302fa589ac064ca16e1031ce
dev <remnev.dev@gmail.com>
2023-04-06 13:52:13 +0000

    feat: add card && style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import '../styles/index.scss';
+import { cardData } from '../utils/constants.js';
 
 const cartInfo = document.querySelector('.cart-info');
 const content = document.querySelector('.content');
@@ -13,3 +14,5 @@ close.addEventListener('click', () => {
   content.classList.remove('content__opened');
   content.classList.add('content__close');
 });
+
+console.log(cardData[0].img);
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const cardData = [
+  { id: 1, name: 'm – 01 digital', price: 4, img: '../images/m-01.jpg' },
+  { id: 1, name: 'm – 01 digital', price: 4, img: '../images/m-02.jpg' },
+  { id: 1, name: 'm – 01 digital', price: 4, img: '../images/m-03.jpg' },
+];
