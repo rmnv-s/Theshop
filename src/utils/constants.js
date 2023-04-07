41a7daf95916df0567e6b3ba7531022d66891e4f
dev <remnev.dev@gmail.com>
2023-04-07 08:17:34 +0000

    feat: add custom scroll

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,10 @@
 export const cardData = [
   { id: 1, name: 'm – 01 digital', price: 4, link: './src/images/m-01.jpg' },
-  { id: 1, name: 'm – 02 digital', price: 4, link: './src/images/m-02.jpg' },
-  { id: 1, name: 'm – 03 digital', price: 4, link: './src/images/m-03.jpg' },
+  { id: 2, name: 'm – 02 digital', price: 4, link: './src/images/m-02.jpg' },
+  { id: 3, name: 'm – 03 digital', price: 4, link: './src/images/m-03.jpg' },
+  { id: 4, name: 'm – 04 digital', price: 4, link: './src/images/m-01.jpg' },
+  { id: 5, name: 'm – 05 digital', price: 4, link: './src/images/m-02.jpg' },
+  { id: 6, name: 'm – 06 digital', price: 4, link: './src/images/m-03.jpg' },
 ];
 
 export const cards = document.querySelector('.cards');
