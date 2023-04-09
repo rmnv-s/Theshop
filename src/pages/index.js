ef6e9d1c4da3406e4b8a027096a5a58b25f193c6
Dev <remnev.dev@gmail.com>
2023-04-09 16:04:07 +0000

    fix: fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,66 @@
 import '../styles/index.scss';
-import { cardData, cards, cartCount } from '../utils/constants.js';
+import { cardData, cards, cartCount, cardProduct } from '../utils/constants.js';
 
 const cartInfo = document.querySelector('.cart-info');
 const cartInner = document.querySelector('.cart-inner');
 const close = document.querySelector('.cart-info__close');
 
 cartInfo.addEventListener('click', () => {
-  cartInner.classList.add('cart-inner__opened');
-  cartInner.classList.remove('cart-inner__close');
+	cartInner.classList.add('cart-inner__opened');
+	cartInner.classList.remove('cart-inner__close');
 });
 
 close.addEventListener('click', () => {
-  cartInner.classList.remove('cart-inner__opened');
-  cartInner.classList.add('cart-inner__close');
+	cartInner.classList.remove('cart-inner__opened');
+	cartInner.classList.add('cart-inner__close');
 });
 
 function getCard(item) {
-  const cardTemplate = document.querySelector('.card-template').content;
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	const cardTemplate = document.querySelector('.card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-  cardElement.querySelector('.card__heding').textContent = item.name;
-  cardElement.querySelector('.card__price').textContent = `$ ${item.price}.00`;
-  const cardPhoto = cardElement.querySelector('.card__img');
-  cardPhoto.src = item.link;
-  cardPhoto.alt = item.name;
+	cardElement.querySelector('.card__heding').textContent = item.name;
+	cardElement.querySelector('.card__price').textContent = `$ ${item.price}.00`;
+	const cardPhoto = cardElement.querySelector('.card__img');
+	cardPhoto.src = item.link;
+	cardPhoto.alt = item.name;
 
-  const cardBtn = cardElement.querySelector('.card__btn');
-  cardBtn.addEventListener('click', () => {
-    console.log('click btn', item.name);
-    cartCount.textContent++;
-    cardBtn.disabled = true;
-    cardBtn.textContent = 'product in cart';
-  });
+	const cardBtn = cardElement.querySelector('.card__btn');
+	cardBtn.addEventListener('click', () => {
+		console.log('click btn', item.name);
+		cartCount.textContent++;
+		cardBtn.disabled = true;
+		cardBtn.textContent = 'product in cart';
+		cardProduct.append(cardToBasket(item));
+	});
 
-  return cardElement;
+	return cardElement;
 }
 
 // функция добавления карточку на страницу
 function addCard(item, container) {
-  const newCard = getCard(item);
-  container.append(newCard);
+	const newCard = getCard(item);
+	container.append(newCard);
 }
 
 // Карточки из коробки
-cardData.forEach(function (item) {
-  addCard(item, cards);
+cardData.forEach(function(item) {
+	addCard(item, cards);
 });
 
 console.log(cartCount.textContent);
+
+function cardToBasket(item) {
+	const cardTemplateBasket = document.querySelector('.product-template').content;
+	const cardElementBasket = cardTemplateBasket.querySelector('.product').cloneNode(true);
+
+	cardElementBasket.querySelector('.card-product__heding').textContent = item.name;
+	cardElementBasket.querySelector('.card-product__price').textContent = `$ ${item.price}.00`;
+
+	const cardRemove = cardElementBasket.querySelector('.card-remove');
+	cardRemove.addEventListener('click', () => {
+		console.log(`card removed: ${item.name}`);
+	});
+
+	return cardElementBasket;
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,12 @@
 export const cardData = [
-  { id: 1, name: 'm – 01 digital', price: 4, link: './src/images/m-01.jpg' },
-  { id: 2, name: 'm – 02 digital', price: 3, link: './src/images/m-02.jpg' },
-  { id: 3, name: 'm – 03 digital', price: 8, link: './src/images/m-03.jpg' },
-  { id: 4, name: 'm – 04 digital', price: 2, link: './src/images/m-01.jpg' },
-  { id: 5, name: 'm – 05 digital', price: 5, link: './src/images/m-02.jpg' },
-  { id: 6, name: 'm – 06 digital', price: 6, link: './src/images/m-03.jpg' },
+	{ id: 1, name: 'm – 01 digital', price: 4, link: './src/images/m-01.jpg' },
+	{ id: 2, name: 'm – 02 digital', price: 3, link: './src/images/m-02.jpg' },
+	{ id: 3, name: 'm – 03 digital', price: 8, link: './src/images/m-03.jpg' },
+	{ id: 4, name: 'm – 04 digital', price: 2, link: './src/images/m-01.jpg' },
+	{ id: 5, name: 'm – 05 digital', price: 5, link: './src/images/m-02.jpg' },
+	{ id: 6, name: 'm – 06 digital', price: 6, link: './src/images/m-03.jpg' },
 ];
 
 export const cards = document.querySelector('.cards');
 export const cartCount = document.querySelector('.cart-info__count');
+export const cardProduct = document.querySelector('.card-product');
