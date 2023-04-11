d900b1b27aa686988629c4135b16ad19e3d1247f
dev <remnev.dev@gmail.com>
2023-04-11 12:05:50 +0000

    fix: remove card

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,74 +6,77 @@ const cartInner = document.querySelector('.cart-inner');
 const close = document.querySelector('.cart-info__close');
 
 cartInfo.addEventListener('click', () => {
-	cartInner.classList.add('cart-inner__opened');
-	cartInner.classList.remove('cart-inner__close');
+  cartInner.classList.add('cart-inner__opened');
+  cartInner.classList.remove('cart-inner__close');
 });
 
 close.addEventListener('click', () => {
-	cartInner.classList.remove('cart-inner__opened');
-	cartInner.classList.add('cart-inner__close');
+  cartInner.classList.remove('cart-inner__opened');
+  cartInner.classList.add('cart-inner__close');
 });
 
 function getCard(item) {
-	const cardTemplate = document.querySelector('.card-template').content;
-	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardTemplate = document.querySelector('.card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-	cardElement.querySelector('.card__heding').textContent = item.name;
-	cardElement.querySelector('.card__price').textContent = `$ ${item.price}.00`;
-	const cardPhoto = cardElement.querySelector('.card__img');
-	cardPhoto.src = item.link;
-	cardPhoto.alt = item.name;
+  cardElement.querySelector('.card__heding').textContent = item.name;
+  cardElement.querySelector('.card__price').textContent = `$ ${item.price}.00`;
+  const cardPhoto = cardElement.querySelector('.card__img');
+  cardPhoto.src = item.link;
+  cardPhoto.alt = item.name;
 
-	const cardBtn = cardElement.querySelector('.card__btn');
-	cardBtn.addEventListener('click', () => {
-		cartCount.textContent++;
-		handleCardBtnAdd(cardBtn);
-		cardProduct.append(cardToBasket(item));
-	});
+  const cardBtn = cardElement.querySelector('.card__btn');
+  cardBtn.addEventListener('click', () => {
+    cartCount.textContent++;
 
-	return cardElement;
+    handleCardBtnAdd(cardBtn);
+    cardProduct.append(cardToBasket(item));
+  });
+
+  return cardElement;
 }
 
 // функция добавления карточку на страницу
 function addCard(item, container) {
-	const newCard = getCard(item);
-	container.append(newCard);
+  const newCard = getCard(item);
+  container.append(newCard);
 }
 // Карточки из коробки
-cardData.forEach(function(item) {
-	addCard(item, cards);
+cardData.forEach(function (item) {
+  addCard(item, cards);
 });
 
 let arr = [];
 function cardToBasket(item) {
-	const cardTemplateBasket = document.querySelector('.product-template').content;
-	const cardElementBasket = cardTemplateBasket.querySelector('.product').cloneNode(true);
+  const cardTemplateBasket = document.querySelector('.product-template').content;
+  const cardElementBasket = cardTemplateBasket.querySelector('.product').cloneNode(true);
 
-	cardElementBasket.querySelector('.card-product__heding').textContent = item.name;
-	cardElementBasket.querySelector('.card-product__price').textContent = `$ ${item.price}.00`;
+  cardElementBasket.querySelector('.card-product__heding').textContent = item.name;
+  cardElementBasket.querySelector('.card-product__price').textContent = `$ ${item.price}.00`;
 
-	const cardRemove = cardElementBasket.querySelector('.card-remove');
-	cardRemove.addEventListener('click', evt => {
-		evt.target.closest('.product').remove();
-		arr.forEach((el, i) => {
-			if (el.id === item.id) arr.splice(i, 1);
-		});
-		console.log(arr);
-	});
+  const cardRemove = cardElementBasket.querySelector('.card-remove');
+  cardRemove.addEventListener('click', (evt) => {
+    evt.target.closest('.product').remove();
+    arr.forEach((el, i) => {
+      if (el.id === item.id) arr.splice(i, 1);
+    });
+    cartCount.textContent--;
 
-	if (arr.find(i => i.id === item.id)) {
-		alert('product added');
-	} else {
-		arr.push(item);
-		console.log(arr);
-	}
-
-	// arr.push(cardElementBasket);
-	return cardElementBasket;
+    const cardBtn = document.querySelector('.card__btn');
+    handleCardBtnRemove(cardBtn);
+  });
+  arr.push(item);
+  return cardElementBasket;
 }
 
 function handleCardBtnAdd(item) {
-	item.disabled = true;
-	item.textContent = 'product in cart';
+  item.disabled = true;
+  item.textContent = 'product in cart';
+  item.style.cursor = 'default';
+}
+
+function handleCardBtnRemove(item) {
+  item.disabled = false;
+  item.textContent = 'add to cart';
+  item.style.cursor = 'pointer';
 }
